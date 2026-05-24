c44064704346d17f00f0c5ccaa31094ef7ba7192
SaaranshDx <saaransh762@gmail.com>
2026-05-24 17:13:18 +0530

    project initialised

---
+++ src/client/kotlin/hex/capes/client/HexClient.kt
@@ -0,0 +1,9 @@
+package hex.capes.client
+
+import net.fabricmc.api.ClientModInitializer
+
+object HexClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
